cee0b4f3c362660e31ab3d36b6fcb4f5847f5b32
Stephen Cleary <stephen.cleary@faithlife.com>
2018-04-27 16:40:23 -0400

    Add basic example.

---
+++ example/TimerSample/TimerSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+using System.Runtime.Loader;
+using System.Threading;
+using System.Threading.Tasks;
+using Faithlife.Ananke;
+
+namespace TimerSample
+{
+    class Program
+    {
+	    private static readonly AnankeSettings Settings = AnankeSettings.Create(maximumRuntime: TimeSpan.FromSeconds(30));
+
+		static void Main() => AnankeRunner.Main(Settings, async context =>
+		{
+			while (true)
+			{
+				await Task.Delay(TimeSpan.FromSeconds(1), context.ExitRequested);
+				Console.WriteLine("Hello World!");
+			}
+		});
+	}
+}
